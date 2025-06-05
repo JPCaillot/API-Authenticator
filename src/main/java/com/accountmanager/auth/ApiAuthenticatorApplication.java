69577e99550daa7e88cc3215225e3011a50737a7
JPCaillot <joaopcaillot@gmail.com>
2025-06-05 15:23:12 -0300

    Initial commit

---
+++ src/main/java/com/accountmanager/auth/ApiAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.accountmanager.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accountmanager/auth/ApiAuthenticatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.accountmanager.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is used to check if the Spring application context loads successfully.
+	}
+
+}
